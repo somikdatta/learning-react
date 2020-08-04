88dae482d7654a51760b8aaf4326dee12195eaf3
Somik Datta <developersomik@gmail.com>
2020-08-04 19:36:46 +0530

    📦 NEW: FontAwesome, Navigation, New tailwind prop erty

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor:{
+        'black-t-50': 'rgba(0,0,0,0.5)'
+      }
+    },
   },
   variants: {},
   plugins: [],
