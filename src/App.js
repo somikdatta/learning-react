29c1994cf5efa9aeb0a8cd682948f337dd8782f6
Somik Datta <developersomik@gmail.com>
2020-08-04 22:02:53 +0530

    📦 NEW: Routing, Views

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+
 import HelloWorld from './Components/HelloWorld'
-// import CounterExample from './Components/CounterExample'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
+import Home from './Views/Home'
+import About from './Views/About'
+
 
 function App() {
   return (
     <div>
-      <Header />
-      <HelloWorld name="Drake"/>
-      {/* <CounterExample /> */}
-      <Footer />
+
+      <Router>
+        <Header />
+
+        <HelloWorld name="Drake"/>
+
+              <Switch>
+                <Route exact path="/">
+                  <Home/>
+                </Route>
+                <Route path="/about">
+                  <About/>
+                </Route>
+              </Switch>
+
+        <Footer />
+      </Router>
+
     </div>
   );
 }
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faBars} from '@fortawesome/free-solid-svg-icons'
 import {useTransition, animated} from 'react-spring'
+import NavigationMenu from './NavigationMenu'
 
 function Navigation(){
     const [showMenu, setShowMenu] = useState(false)
@@ -43,12 +44,9 @@ function Navigation(){
                         key={key}
                         style={props}
                         className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow">
-                            <span className="font-bold">
-                                The menu
-                            </span>
-                            <ul>
-                                <li>Home</li>
-                            </ul>
+                            <NavigationMenu 
+                                closeMenu={()=>setShowMenu(false)}
+                            />
                     </animated.div>
                 )
             }
