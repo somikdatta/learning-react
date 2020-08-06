54535b88543b3db3c88adaa2cef2577d67038d13
Somik Datta <developersomik@gmail.com>
2020-08-06 19:02:54 +0530

    👌 IMPROVE: Axios handle errors

--- src/Views/Product.js
+++ src/Views/Product.js
@@ -5,34 +5,51 @@ import SkeletonLoader from '../Components/SkeletonLoader';
 function Product(){
     const {id} = useParams();
     const url=`https://5f2a8d0d6ae5cc0016422a91.mockapi.io/api/v1/products/${id}`
-    const [product,setProduct] = useState(null)
+    const [product,setProduct] = useState({
+        data: null,
+        error: false
+    })
 
     let content = <SkeletonLoader/>
 
     useEffect(()=>{
         axios.get(url)
-        .then(res=>{
-            setProduct(res.data)
-        })
+            .then(res=>{
+                setProduct({
+                    data: res.data,
+                    error: false
+                })
+            })
+            .catch(()=>{
+                setProduct({
+                    data: null,
+                    error: true
+                })
+            })
     },[url])
 
-    if(product){
+    if(product.error){
+        content = <p>
+            There was an error, please refresh or try again later!
+        </p>
+    }
+    if(product.data){
         content =
         <div>
             <h1 className="text-2xl font-bold mb-3">
-                {product.name}
+                {product.data.name}
             </h1>
             <div>
                 <img
-                    src={product.images}
-                    alt={product.name}
+                    src={product.data.images}
+                    alt={product.data.name}
                 />
             </div>
             <div className="font-bold text-xl mb-3">
-                ₹ {parseFloat(product.price) * 74.97}
+                ₹ {parseFloat(product.data.price) * 74.97}
             </div>
             <div>
-                {product.description}
+                {product.data.description}
             </div>
         </div>
     }
